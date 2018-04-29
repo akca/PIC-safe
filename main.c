b6448bf856862b52ba2b4f78130278942049853c
Fatih Akca <fatihakca5@gmail.com>
2018-04-30 00:42:20 +0300

    7-seg display flickering is almost gone, timers calibrated

--- main.c
+++ main.c
@@ -21,13 +21,15 @@ unsigned char rbX;
 unsigned char preval;
 unsigned char rb_int_received;
 unsigned char pin[4];
-unsigned char counter_test_period;
 unsigned char after_blink;
-unsigned char blink_active;
+unsigned char display_active;
+char counter_test_period;
 
 void init_system();
 void RE1_task();
-void sec_delay();
+void delay_1sec();
+void delay_4p5msec();
+void delay_10msec();
 void init_tmr0_interrupt();
 void init_rb_interrupt();
 void init_ADC();
@@ -42,7 +44,7 @@ void display_remaining_time();
 void main(void) {
 
   init_system();
-
+  
   WriteCommandToLCD(0x81); // Goto to the 2nd char of the first line
   WriteStringToLCD("$>Very  Safe<$");
   WriteCommandToLCD(0xC1);
@@ -53,9 +55,9 @@ void main(void) {
   }
 
   // 3 second delay using loops
-  sec_delay();
-  sec_delay();
-  sec_delay();
+  delay_1sec();
+  delay_1sec();
+  delay_1sec();
 
   ClearLCDScreen();
   WriteCommandToLCD(0x81); // Goto to the 2nd char of the first line
@@ -93,7 +95,7 @@ void main(void) {
       }
 
       if (rb_int_received) {
-        __delay_ms(10);
+        delay_10msec(); // debouncing
         if (preval == (PORTB & (0x01 << rbX)) && preval == 0) {
           if (rbX == 6)
             rb6_push = 1;
@@ -109,9 +111,6 @@ void main(void) {
   }
 
   preval = 0;
-  PIE1bits.ADIE = 0;
-  ADCON0bits.ADON = 0;
-  INTCONbits.PEIE = 0;
   rb7_push = 0;
   rbX = 7;
   INTCONbits.RBIF = 0;
@@ -119,7 +118,7 @@ void main(void) {
 
   while (!rb7_push) {
     if (rb_int_received) {
-      __delay_ms(10);
+      delay_10msec(); // debouncing
       if (preval == (PORTB & (0x01 << rbX)) && preval == 0) {
         if (rbX == 6)
           rb6_push = 1;
@@ -150,18 +149,13 @@ void main(void) {
       break;
   }
 
-  ADCON0bits.ADON = 1;
-  PIR1bits.ADIF = 0;
-  PIE1bits.ADIE = 1;
-  INTCONbits.PEIE = 1;
-
   counter_test_period = 120;
   T1CON = 0;
   TMR1 = 0;
   T1CONbits.RD16 = 1;    // 16-bit mode
   T1CONbits.T1CKPS0 = 1; // 1:8 prescaler
   T1CONbits.T1CKPS1 = 1;
-  IPR1bits.TMR1IP = 1; // high priority
+  IPR1bits.TMR1IP = 1;   // high priority
   PIR1bits.TMR1IF = 0;
   PIE1bits.TMR1IE = 1;
   T1CONbits.TMR1ON = 1;
@@ -169,11 +163,14 @@ void main(void) {
   while (counter_test_period > 0) {
 
     if (!enter_pin()) {
-      WriteCommandToLCD(0x8B); // Goto to the 2nd char of the first line
+      if (counter_test_period <= 0)
+        break;
+      
+      WriteCommandToLCD(0x8B);
       WriteStringToLCD("XXXX");
       WriteCommandToLCD(0xC0);
       WriteStringToLCD("Try after 20sec.");
-      unsigned char suspend_start = counter_test_period;
+      char suspend_start = counter_test_period;
 
       while (1) {
         display_remaining_time();
@@ -185,13 +182,15 @@ void main(void) {
           break;
       }
     } else {
-      WriteCommandToLCD(0x80); // Goto to the 2nd char of the first line
+      WriteCommandToLCD(0x80);
       WriteStringToLCD("Safe is opening!");
       WriteCommandToLCD(0xC0);
       WriteStringToLCD("$$$$$$$$$$$$$$$$");
 
-      while (1)
-        ;
+      while (1) {
+        PIE1bits.TMR1IE = 0;
+        display_remaining_time();
+      }
     }
   }
   PIE1bits.ADIE = 0;
@@ -200,7 +199,7 @@ void main(void) {
 }
 
 unsigned char enter_pin() {
-  blink_active = 1;
+  display_active = 1;
   unsigned char entered_pin[4];
 
   ClearLCDScreen();
@@ -225,7 +224,7 @@ unsigned char enter_pin() {
       hash_flag = 1;
       adc_finish = 0;
 
-      while (!adc_finish) { // while(1)
+      while (!adc_finish) {
         display_remaining_time();
 
         if (blink_flag == 1) {
@@ -233,7 +232,7 @@ unsigned char enter_pin() {
           blink_digit(digitno);
         }
 
-        if (counter_test_period < 0)
+        if (counter_test_period <= 0)
           return 0;
       }
 
@@ -257,7 +256,7 @@ unsigned char enter_pin() {
         }
 
         if (rb_int_received) {
-          __delay_ms(10);
+          delay_10msec(); // debouncing
           if (preval == (PORTB & (0x01 << rbX)) && preval == 0) {
             if (rbX == 6)
               rb6_push = 1;
@@ -270,7 +269,6 @@ unsigned char enter_pin() {
 
       entered_pin[digitno] = ADC_value;
       INTCONbits.RBIE = 0; // disable rb interrupt
-      //__delay_ms(100);
     }
 
     preval = 0;
@@ -288,7 +286,7 @@ unsigned char enter_pin() {
       if (counter_test_period <= 0)
         return 0;
       if (rb_int_received) {
-        __delay_ms(10);
+        delay_10msec(); // debouncing
         if (preval == (PORTB & (0x01 << rbX)) && preval == 0) {
           if (rbX == 6)
             rb6_push = 1;
@@ -299,20 +297,23 @@ unsigned char enter_pin() {
       }
     }
 
-    // check pin
+    
 
     INTCONbits.RBIE = 0;
 
+    // check entered pin is correct
     int digit;
     for (digit = 0; digit < 4; digit++) {
       if (pin[digit] != entered_pin[digit])
         break;
     }
 
+    // pin is correct
     if (digit == 4)
       return 1;
   }
-  // no attempts left
+  
+  // no attempts left, fail
   return 0;
 }
 
@@ -366,42 +367,52 @@ void init_system() {
   PORTH &= 0x0F;
 }
 
-void sec_delay() // 1 second delay
+void delay_1sec() // 1 second of delay
 {
-  unsigned char t1 = 0x50;
-  unsigned char t2 = 0x90;
-  unsigned char t3 = 0x1F;
+  int t1 = 1000;
+  int t2 = 1248;
 
-  while (t3 != 0) {
+  while (t1 != 0) {
     while (t2 != 0) {
-      while (t1 != 0) {
-        t1--;
-      }
-      t1--;
       t2--;
     }
-    t2--;
-    t3--;
+    t1--;
+    t2 = 1248;
+  }
+}
+
+void delay_4p5msec() // 4.5 milliseconds of delay
+{
+  int t = 4999;
+
+  while (t != 0) {
+    t--;
+  }
+}
+
+void delay_10msec() // 10 millisecond of delay
+{
+  int t = 11110;
+
+  while (t != 0) {
+    t--;
   }
 }
 
 void init_tmr0_interrupt() {
   counter = 0;
 
-  T0CON = 0b01000111; // T0CON = b'01000111'
+  T0CON = 0b01000111;
   TMR0 = 0;
 
-  INTCON2bits.TMR0IP = 1; // bsf INTCON2, 2
-                          // Timer0 set as HIGH priority
+  INTCON2bits.TMR0IP = 1; // Timer0 set as HIGH priority
   INTCONbits.TMR0IF = 0;
   INTCONbits.TMR0IE = 1;
-  // INTCONbits.PEIE = 1;
-
-  T0CONbits.TMR0ON = 1; // Enable Timer0 by setting TMR0ON
+  T0CONbits.TMR0ON = 1;   // Enable Timer0 by setting TMR0ON
 }
 
 void init_rb_interrupt() {
-  TRISB4 = 0; // rb4 & rb5 - outputs (initLCD())
+  TRISB4 = 0;           // rb4 is output
   TRISB6 = 1;
   TRISB7 = 1;           // rb6 & rb7 - inputs
   INTCON2bits.RBIP = 1; // set rb as high-priority interrupt
@@ -412,7 +423,7 @@ void init_rb_interrupt() {
 }
 
 void init_ADC() {
-  ADCON1 = 0; // TODO: check other channels
+  ADCON1 = 0;           // TODO: check other channels
   ADCON0 = 0;
   ADCON0 = ADCON0 | 0b00110000;
   ADCON2 = 0b10010010;
@@ -427,6 +438,7 @@ void interrupt high_priority high_isr() {
   if (INTCONbits.TMR0IE && INTCONbits.TMR0IF) {
     counter++;           // increment counter variable
     counter2++;          // increment counter variable
+    
     if (counter == 38) { // 250 ms
       counter = 0;
       blink_flag = 1;
@@ -435,11 +447,14 @@ void interrupt high_priority high_isr() {
         _500ms_passed = 1;
       }
     }
-    if (counter2 == 25) { // TODO should be adjusted to get 100 ms
+    
+    if (counter2 == 15) { // 100 ms
       counter2 = 0;
       ADCON0bits.GO = 1;
     }
+    
     INTCONbits.TMR0IF = 0; // Clear TMROIF
+    
   } else if (INTCONbits.RBIE && INTCONbits.RBIF) {
 
     preval = PORTB & (0x01 << rbX);
@@ -448,12 +463,16 @@ void interrupt high_priority high_isr() {
     // 0, i.e. pressed
     rb_int_received = 1;
     INTCONbits.RBIF = 0;
+    
   } else if (PIE1bits.TMR1IE && PIR1bits.TMR1IF) {
+    
     counter1sec++;
-    if (counter1sec == 19) {
+    
+    if (counter1sec == 19) { // 1 second delay
       counter1sec = 0;
       counter_test_period--;
     }
+    
     PIR1bits.TMR1IF = 0;
   }
 }
@@ -468,9 +487,6 @@ void interrupt low_priority low_isr() {
 
     firstrun_flag = 0;
     PIR1bits.ADIF = 0;
-
-    //        WriteCommandToLCD(0x81);    // Goto to the 2nd char of the first
-    //        line WriteDataToLCD((int)'0' + map_ADC_value(ADC_value));
   }
 }
 
@@ -480,29 +496,28 @@ void blink_digit(int digit_address) {
     WriteCommandToLCD(digit_address + 0x8B);
     after_blink = 1;
 
-    if (blink_active)
+    if (display_active)
       display_remaining_time();
 
     WriteDataToLCD(' ');
 
-    if (blink_active)
+    if (display_active)
       display_remaining_time();
 
     after_blink = 0;
     hash_flag = 0;
 
-  }
-  else if (hash_flag == 0) {
+  } else if (hash_flag == 0) {
 
     WriteCommandToLCD(digit_address + 0x8B);
     after_blink = 1;
 
-    if (blink_active)
+    if (display_active)
       display_remaining_time();
 
     WriteDataToLCD('#');
 
-    if (blink_active)
+    if (display_active)
       display_remaining_time();
 
     after_blink = 0;
@@ -538,27 +553,11 @@ void display_remaining_time() {
 
   PORTJ = 0x00;
   PORTH = 0xff;
-  int divisor = 100;
+  int divisor = 1000;
   unsigned char value = counter_test_period;
   unsigned char tmp;
 
   for (int digit = 0; digit < 4; digit++) {
-    if (digit == 0) {
-      
-//      if (after_blink == 1)
-//        __delay_ms(0);
-//      else
-        __delay_ms(4);
-      
-      PORTH &= 0b11110000;
-      PORTH |= 0x01;
-      PORTJ = 0b00111111;
-
-      continue;
-    }
-    
-    __delay_ms(4);
-    PORTH &= 0b11110000;
 
     value = value / divisor;
 
@@ -595,12 +594,12 @@ void display_remaining_time() {
       break;
     }
 
+    PORTH &= 0b11110000;
     PORTH |= 0x01 << digit;
     PORTJ = tmp;
-    
-    if (digit == 3)
-      __delay_ms(4);
-    
+
+    delay_4p5msec(); // 7 segment display flicker avoidance
+
     value = counter_test_period % divisor;
     divisor /= 10;
   }
